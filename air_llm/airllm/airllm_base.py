3ab866b3c617c0a976e76a4e73bc14f0356ac391
Codys12 <41397239+Codys12@users.noreply.github.com>
2024-08-05 20:58:06 -0500

    .

--- air_llm/airllm/airllm_base.py
+++ air_llm/airllm/airllm_base.py
@@ -425,6 +425,7 @@ class AirLLMBaseModel(GenerationMixin):
                 elif layer_name == self.layer_names_dict['lm_head']:
                     logits = self.run_lm_head(layer, hidden_states, top_k)
                 else:
+                    print(hidden)
                     print(layer)
                     layer.to(self.running_device)
                     layer_outputs = layer(
