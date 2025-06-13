e0e653d42ff6ec04210fd6f1ada67993b388a4f9
Codys12 <41397239+Codys12@users.noreply.github.com>
2025-06-13 08:06:53 -0500

    Update __init__.py

--- air_llm/__init__.py
+++ air_llm/__init__.py
@@ -9,14 +9,14 @@ if is_on_mac_os:
     from .airllm_llama_mlx import AirLLMLlamaMlx
     from .auto_model import AutoModel
 else:
-    from .airllm import AirLLMLlama2
-    from .airllm_chatglm import AirLLMChatGLM
-    from .airllm_qwen import AirLLMQWen
-    from .airllm_baichuan import AirLLMBaichuan
-    from .airllm_internlm import AirLLMInternLM
-    from .airllm_mistral import AirLLMMistral
-    from .airllm_mixtral import AirLLMMixtral
-    from .airllm_base import AirLLMBaseModel
-    from .auto_model import AutoModel
-    from .utils import split_and_save_layers
-    from .utils import NotEnoughSpaceException
+    from .airllm.airllm import AirLLMLlama2
+    from .airllm.airllm_chatglm import AirLLMChatGLM
+    from .airllm.airllm_qwen import AirLLMQWen
+    from .airllm.airllm_baichuan import AirLLMBaichuan
+    from .airllm.airllm_internlm import AirLLMInternLM
+    from .airllm.airllm_mistral import AirLLMMistral
+    from .airllm.airllm_mixtral import AirLLMMixtral
+    from .airllm.airllm_base import AirLLMBaseModel
+    from .airllm.auto_model import AutoModel
+    from .airllm.utils import split_and_save_layers
+    from .airllm.utils import NotEnoughSpaceException
